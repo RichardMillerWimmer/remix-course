8944d6d61f0bfbc299bbd758777eb14be2d2dda6
Richard Miller <richardmmill@gmail.com>
2023-05-28 11:40:52 -0400

    a tag and Link difference in remix

--- app/routes/demo.tsx
+++ app/routes/demo.tsx
@@ -1,3 +1,11 @@
+import { Link } from "@remix-run/react"
+
 export default function DemoPage() {
-    return <div><h1>Demo Page</h1></div>
+    return (
+        <div>
+            <h1>Demo Page</h1>
+            {/* Link makes no request to the server for the html, this will run client side and provide a SAP experience.  */}
+            <Link to="/">Home Page</Link>
+        </div>
+    )
 }
